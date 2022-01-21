eb1e91828f8c98ab4878d2fa3fd97afac342d899
Thomas Vargas <rodrigues.pauloricardo@gmail.com>
2022-01-20 22:21:57 -0300

    at

---
+++ caixa.js
@@ -0,0 +1,27 @@
+var saldo = 2000;
+var deposito = 500;
+var saque = -100;
+var sairA = '';
+
+var opcao = document.getElementById('saldo');
+var opcao1 = document.getElementById('saque');
+var opcao2 = document.getElementById('deposito');
+
+function saldoA(){  
+    if(opcao.value === 'SALDO'){  
+        msg=('Seu saldo é R$ ' + saldo)
+    document.getElementById('res').innerHTML = ('Seu saldo é R$ ' + saldo)
+}
+}
+function saqueA(){  
+    if(opcao1.value === 'SAQUE') { 
+        msg=('Seu saque foi de R$ ' + saque)
+    document.getElementById('res').innerHTML = ('Seu saque foi de R$ ' + saque)
+}
+}
+function depositoA(){  
+    if(opcao2.value === 'DEPOSITO') {
+        msg=('Seu depósito foi de é R$ ' + deposito)        
+    document.getElementById('res').innerHTML = ('Seu depósito foi de é R$ ' + deposito)
+}
+}
