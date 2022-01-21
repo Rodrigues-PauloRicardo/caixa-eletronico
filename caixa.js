cfb2c1ea1dbe14a33180e8261d96729b0fbd23b3
Thomas Vargas <rodrigues.pauloricardo@gmail.com>
2022-01-21 00:23:12 -0300

    at

--- caixa.js
+++ caixa.js
@@ -9,19 +9,16 @@ var opcao2 = document.getElementById('deposito');
 
 function saldoA(){  
     if(opcao.value === 'SALDO'){  
-        msg=('Seu saldo é R$ ' + saldo)
     document.getElementById('res').innerHTML = ('Seu saldo é R$ ' + saldo)
 }
 }
 function saqueA(){  
     if(opcao1.value === 'SAQUE') { 
-        msg=('Seu saque foi de R$ ' + saque)
     document.getElementById('res').innerHTML = ('Seu saque foi de R$ ' + saque)
 }
 }
 function depositoA(){  
     if(opcao2.value === 'DEPOSITO') {
-        msg=('Seu depósito foi de é R$ ' + deposito)        
     document.getElementById('res').innerHTML = ('Seu depósito foi de é R$ ' + deposito)
 }
 }
